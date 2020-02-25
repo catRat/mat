fc0e5ce3dccebdcc6543fe96f313b849b3ab58b6
周明东 <505732590@qq.com>
2020-02-25 11:14:37 +0800

    first commit

---
+++ mat1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	FILE *fp;
+	const char* SS = 
+		"\\documentclass[oneside]{book}\n"
+		"\\usepackage{xeCJK, indentfirst, makeidx}\n"
+		"\\usepackage[breaklinks, colorlinks, linkcolor=black, citecolor=black, urlcolor=black]{hyperref}\n"
+		"\\setCJKmainfont[BoldFont=Noto Serif CJK SC Bold, ItalicFont=Noto Serif CJK SC Light]{Noto Serif CJK SC}\n"
+		"\\setCJKsansfont[BoldFont=Noto Sans CJK SC Bold, ItalicFont=Noto Sans CJK SC Light]{Noto Sans CJK SC}\n"
+		"\\setCJKmonofont[BoldFont=Noto Sans Mono CJK SC Bold, ItalicFont=Noto Sans Mono CJK SC]{Noto Sans Mono CJK SC}\n"
+		"\\setlength{\\parindent}{2em}\n"
+		"\\usepackage[hmargin=1.25in, vmargin=1in]{geometry}\n"
+		"\\usepackage{enumitem}\n"
+		"\\newcommand{\\tabincell}[2]{\\begin{tabular}{@{}#1@{}}#2\\end{tabular}}\n"
+		"\\makeindex\n"
+		"\\title{xxx}\n"
+		"\\begin{document}\n"
+		"\\maketitle\n"
+		"\\tableofcontents\n"
+		"\\end{document}\n";
+
+	if ((fp = fopen("tex.tex", "w")) == NULL)
+	{
+		fprintf(stdout, "Can't open \"Makefile\"\n");
+		exit(1);
+	}
+	fputs(SS, fp);
+	if(fclose(fp) != 0)
+		fprintf(stderr, "Error closing file\n");
+	printf("done.\n");
+
+}
